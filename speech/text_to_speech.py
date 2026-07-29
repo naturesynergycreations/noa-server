58dcab462a5fc658a8388475d9aab164c187b718
NSC <naturesynergycreations2025@gmail.com>
2026-07-29 22:42:23 +0530

    Short reply test

--- speech/text_to_speech.py
+++ speech/text_to_speech.py
@@ -15,18 +15,18 @@ async def generate(text):
     await communicate.save(MP3_FILE)
 
     subprocess.run([
-    "ffmpeg",
-    "-y",
-    "-i",
-    MP3_FILE,
-    "-ac",
-    "1",
-    "-ar",
-    "16000",
-    "-sample_fmt",
-    "s16",
-    WAV_FILE
-], check=True)
+        "ffmpeg",
+        "-y",
+        "-i",
+        MP3_FILE,
+        "-acodec",
+        "pcm_s16le",
+        "-ac",
+        "1",
+        "-ar",
+        "16000",
+        WAV_FILE
+    ], check=True)
 
     print("reply.wav Created")
 
